2994aed4f13ff18a825d970149a8aeb6877e4478
yorwel <nusebacra@gmail.com>
2024-06-25 00:58:34 +0800

    c

--- pages/11_Scoring.py
+++ pages/11_Scoring.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 
 # prep validation data
-data_validation_filepath = "LLM_Test\data_validation.csv"
+data_validation_filepath = "data_validation.csv"
 
 data_validation = pd.read_csv(data_validation_filepath, dtype = str)
 
